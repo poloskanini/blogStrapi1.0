98505d094976150cf7ce9872ac0c6a98a5dc560b
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-09-05 14:19:28 +0200

    git init

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  // ...
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: 'slug',
+          references: 'title',
+        },
+      },
+    },
+  },
+  // ...
+});
